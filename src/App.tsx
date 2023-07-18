b13f0dfa8825503fbaebd8f7b1027122fe537d05
Han Chen <hanmikechen@gmail.com>
2023-07-18 17:39:16 -0400

    add profile pic

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import './App.css';
 import particlesOptions from "./particles.json";
 import { ISourceOptions } from "tsparticles-engine";
 import Navbar from "./Navbar";
+import profilePhoto from "../src/assets/images/profile_photo.jpg";
 
 function App() {
     const particlesInit = useCallback(async (engine: Engine) => {
@@ -16,6 +17,12 @@ function App() {
         <div className="App">
             <Particles options={particlesOptions as ISourceOptions} init={particlesInit}/>
             <Navbar/>
+            <div className="main-container flex flex-col items-center w-full">
+                <img className="w-52 rounded-full aspect-square mt-24" src={profilePhoto} alt="han" />
+                <span className="text-white text-4xl font-semibold font-Mont mt-8">
+                    Han Chen
+                </span>
+            </div>
         </div>
     );
 }
--- src/Navbar/index.tsx
+++ src/Navbar/index.tsx
@@ -2,12 +2,12 @@ import './Navbar.css';
 
 const Navbar = (props: any) => {
     return (
-        <div className="flex flex-row-reverse gap-7 font-Mont pr-10 w-screen h-20 text-white">
-            <button>Contact</button>
-            <button>Skills</button>
-            <button>Experience</button>
-            <button>Projects</button>
-            <button>Home</button>
+        <div className="flex items-center flex-row-reverse gap-7 font-Mont pr-12 w-screen h-20 text-white">
+            <a href='#contact'>Contact</a>
+            <a href='#skills'>Skills</a>
+            <a href='#experience'>Experience</a>
+            <a href='#projects'>Projects</a>
+            <a href='#top'>Home</a>
         </div>
     );
 }
