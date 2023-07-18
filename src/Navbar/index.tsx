2672dbfa595220753423f1eec149f4af44965836
Han Chen <hanmikechen@gmail.com>
2023-07-18 16:34:25 -0400

    implement Montserrat font

--- src/Navbar/index.tsx
+++ src/Navbar/index.tsx
@@ -2,7 +2,7 @@ import './Navbar.css';
 
 const Navbar = (props: any) => {
     return (
-        <div className="flex flex-row-reverse gap-7 pr-10 w-screen h-20 text-white">
+        <div className="flex flex-row-reverse gap-7 font-Mont pr-10 w-screen h-20 text-white">
             <button>Contact</button>
             <button>Skills</button>
             <button>Experience</button>
