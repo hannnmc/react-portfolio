0b69913693919100a7704e47496d5b7d048a054c
Han Chen <hanmikechen@gmail.com>
2023-08-10 16:30:13 -0400

    implement the theme setting button

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import Particles from "react-particles";
 import type { Engine } from "tsparticles-engine";
 import { loadFull } from "tsparticles";
@@ -7,14 +7,25 @@ import particlesOptions from "./particles.json";
 import { ISourceOptions } from "tsparticles-engine";
 import Navbar from "./Navbar";
 import profilePhoto from "../src/assets/images/profile_photo.jpg";
+import { motion, AnimatePresence } from 'framer-motion';
+
+// Import the Switch component
+import Switch from "./SwitchTheme";
 
 function App() {
     const particlesInit = useCallback(async (engine: Engine) => {
         await loadFull(engine);
     }, []);
+    const [isOn, setIsOn] = useState(false);
 
     return (
         <div className="App">
+            {/* Add the theme toggle switch */}
+            <Switch  
+            isOn={isOn}
+            setIsOn={setIsOn}
+            />
+
             <Particles options={particlesOptions as ISourceOptions} init={particlesInit}/>
             <Navbar/>
             <div className="main-container flex flex-col items-center w-full">
---
+++ src/SwitchTheme/index.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+import "./SwitchTheme.css";
+
+interface SwitchThemeProps {
+  isOn: boolean;
+  setIsOn: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const SwitchTheme: React.FC<SwitchThemeProps> = ({ isOn, setIsOn }) => {
+
+  
+  return (
+    <div 
+    className='switch-container absolute top-6 left-6'
+    data-darkmode={isOn}
+    >
+      <div 
+        className={`theme-switch ${isOn ? "darkmode" : ""}`} 
+        
+        onClick={() => setIsOn(!isOn)}
+        style={{ justifyContent: isOn ? 'flex-end' : 'flex-start' }}
+      >
+        <motion.div layout className="theme-switch-knob">
+          <AnimatePresence mode='wait' initial={false}>
+            <motion.i
+              className={`theme-icon far fa-${isOn ? 'moon' : 'sun'}`}
+              key={isOn ? 'moon' : 'sun'}
+              initial={{ y: -30, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              exit={{ y: 30, opacity: 0 }} 
+              transition={{ duration: 0.2 }}
+            />
+          </AnimatePresence>
+        </motion.div>
+      </div>
+    </div>
+  );
+}
+
+export default SwitchTheme;
