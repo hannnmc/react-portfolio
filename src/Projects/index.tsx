f4676907b56c8044de470bda33e247717c383c3b
Han Chen <hanmikechen@gmail.com>
2023-08-12 15:44:30 -0400

    apply box shadow to project cards

--- src/Projects/index.tsx
+++ src/Projects/index.tsx
@@ -12,7 +12,7 @@ const wrapperVariants = {
     transition: { duration: .4 }
   },
   animate: {
-    clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)',
+     clipPath: 'polygon(-20px -20px, calc(100% + 20px) -20px, calc(100% + 20px) calc(100% + 20px), -20px calc(100% + 20px))',
     transition: { duration: .4, staggerChildren: .1 }
   },
   exit: {
@@ -20,10 +20,11 @@ const wrapperVariants = {
     transition: { duration: .4 }
   }
 }
+
 const squareVariants = {
   initial: {
     opacity: .6,
-    scale: .8,
+    scale: 0,
   },
   animate: {
     opacity: 1,
@@ -47,7 +48,7 @@ const Projects = () => {
             title: "Cozybnb",
             img: project1photo,
             desc: "Cozybnb is a Full stack Airbnb replica with core interactive features like browsing properties, making reservations, and writing reviews. It's built utilizing React/Redux front-end (JavaScript) and Ruby on Rails back-end while incorporating Authentication, Google Maps, Places, Geocoding APIs, and AWS S3 for a seamless user experience. Leveraging Media Queries and Flexbox CSS, Cozybnb is fully web-responsive and mobile friendly.",
-            live: "sampleurl",
+            live: "https://cozy-bnb.herokuapp.com/",
             github: "https://github.com/hannnmc/Cozybnb"
         },
         {
@@ -81,7 +82,7 @@ const Projects = () => {
     >
         <img className='rounded-t-2xl' src={project.img} alt="" />
         <div className='p-4 flex flex-col items-center'>
-            <span className='text-l font-medium'>{project.title}</span>
+            <span className='text-l font-semibold'>{project.title}</span>
             {/* <span 
                 className='text-base project-desc flex-grow'>{project.desc}
             </span> */}
@@ -91,12 +92,12 @@ const Projects = () => {
     ));
   }
   return (
-    <div id='projects' className={`justify-self-center mt-60 mb-60 cp-transition cp-transition__container cp-transition__container--${selectedProject ? selectedProject.name : ""}`}>
+    <div id='projects' className={`justify-self-center mt-60 mb-60 cp-transition cp-transition__container`}>
       <AnimatePresence mode='wait' initial={false}>
         {selectedProject 
           ? (
             <motion.div 
-              className={`card card__wrapper card__wrapper--${selectedProject.name} font-Mont rounded-2xl`}
+              className={`card font-Mont rounded-2xl`}
               key="card"
               variants={wrapperVariants}
               initial="initial"
@@ -104,17 +105,25 @@ const Projects = () => {
               exit="exit"
             >
             <img className="project-card-img rounded-l-2xl" src={selectedProject.img} alt="" />
-            <div className='w-full p-12 pt-6 flex flex-col'>
+            <div className='w-full px-12 py-6 flex flex-col overflow-visible'>
                 <button className='self-end' onClick={() => setSelectedProject(null)}>
                     <i className="fas fa-times fa-2x"/>
                 </button>
                 <div className="card__header flex justify-between mb-4">
                     <h2>{selectedProject.title}</h2>
                 </div>
-                {/* <div className="card__content"></div> */}
-                <div className="card__text-placeholder w-full text-2xl">
+                <div className="card__text-placeholder w-full text-xl leading-base">
                     {selectedProject.desc}
                 </div>
+                <div className='card-buttons flex justify-between w-full mt-auto'>
+                    <button onClick={() => window.open(selectedProject.github, '_blank')} className='mt-2 w-20 h-7 bg-stone-300 text-slate-900 rounded-2xl'>
+                        CODE
+                    </button>
+                    <button onClick={() => window.open(selectedProject.live, '_blank')} className='mt-2 w-20 h-7 bg-blue-700 text-white rounded-2xl'>
+                        LIVE
+                    </button>
+                
+                </div> 
             </div>
             </motion.div>
           )
@@ -138,3 +147,4 @@ const Projects = () => {
 
  
 export default Projects;
+
