70caad12a0c08cf6787fc228f9a20c7777b26d1a
Han Chen <hanmikechen@gmail.com>
2023-09-24 23:35:57 -0400

    update intro

--- src/Intro/index.tsx
+++ src/Intro/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Intro = () => {
 
     const introduction1 = 
-    "I'm a Full Stack Software Developer at Stellar and a former Data Instructor and Account Executive. My software development journey began when I took a decisive step and joined App Academy in the summer of 2022. Since then, I've thoroughly enjoyed learning new technologies and the feeling of finally figuring out the solution to a problem."
+    "I'm a Full stack software engineer and I love creating stunning and useful web-applications."
 
 
     const introduction2 = 
