93a379e80cd65bfde43ac78070425a0b44678d55
Han Chen <hanmikechen@gmail.com>
2026-08-13 23:00:20 -0400

    update resume

--- src/Experiences/index.tsx
+++ src/Experiences/index.tsx
@@ -56,7 +56,7 @@ const Experiences = () => {
     const experiences = [
         {
             time: "Jun. 2025 - Present",
-            title: "Operations Engineer",
+            title: "Automation Operation Engineer",
             company: "Arch",
             flow: false
         },
