f8b1c9a2d69d5083ce3a6d717a77c4ae502605f5
Han Chen <hanmikechen@gmail.com>
2026-09-23 22:42:43 -0400

    update intro

--- src/Intro/index.tsx
+++ src/Intro/index.tsx
@@ -3,12 +3,7 @@ import React from "react";
 const Intro = () => {
 
     const introduction1 = 
-    "I'm a Full Stack Software Engineer based in NYC with a passion for using technology to solve real world problems. I thrive on building beautiful and functional tools that make a real impact."
-
-
-    // const introduction2 = 
-    // "When I'm not coding, I enjoy taking my Siamese cat for a walk around the neighborhood. I also enjoy rock climbing, hiking, good tv shows and movies."
-    
+    "High-growth startup Full Stack Developer based in NYC. Focused on understanding how the business operates, finding opportunities to improve it, and rapidly turning those opportunities into high-impact builds."
 
     return ( 
         <div className="self-center mt-24 w-1/2 text-xl text-center font-Mont sm:w-3/4 sm:mt-10 sm:text-base">
